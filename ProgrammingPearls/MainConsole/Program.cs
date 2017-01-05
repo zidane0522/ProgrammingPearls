c7e79facc3eae6c8840755f04681b52e0d75d8e3
zidane0522 <lishen1990@163.com>
2017-01-05 18:36:21 +0800

    不重复随机数数组生成

--- ProgrammingPearls/MainConsole/Program.cs
+++ ProgrammingPearls/MainConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 
 namespace MainConsole
 {
@@ -10,6 +11,17 @@ namespace MainConsole
     {
         static void Main(string[] args)
         {
+            DataArrayCls dc = new DataArrayCls();
+            Console.WriteLine(dc.InitMillionArray());
+            Console.WriteLine(dc.InitMillionList());
+            Console.WriteLine(dc.LieBiaoShengCheng());//极慢，估计需要数十分钟，或数小时
+
+            Console.WriteLine(dc.YuanSuGuDing());//一分钟左右
+            Console.WriteLine(dc.SuoJianWeiBu());//一秒左右
+
+            Console.WriteLine(dc.YiDongShuZu());//极慢，估计需要数小时甚至数天
+            Console.WriteLine(dc.ZiShenXiuGai());//一秒左右,比SuoJianWeiBu方法稍快十分之一
+            Console.ReadKey();
         }
     }
 }
